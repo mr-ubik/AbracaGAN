6183fc67e355c5428b106f619ba3cb51d951ad6f
Michele "Ubik" De Simoni <ubik@ubik.tech>
2018-10-08 16:22:23 +0200

    Post DevFest Milano 2018

---
+++ README.md
@@ -0,0 +1,7 @@
+# AbracaGAN: introduction to the almost magical world of GANs
+
+## Contents
+
+- Slides
+- [Code](https://github.com/mr-ubik/AbracaGAN/blob/master/abracagan.py)
+- [TensorFLow Team DCGAN Implementation](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/eager/python/examples/generative_examples/dcgan.ipynb)
--- abracagan.py
+++ abracagan.py
@@ -1,6 +1,7 @@
 """
 Simple DCGAN for the AbracaGAN talk.
 """
+import os
 from typing import Dict
 
 import tensorflow as tf
@@ -9,11 +10,45 @@ import tensorflow.keras as k
 
 tf.enable_eager_execution()
 
-HYPER = {"batch_size": 128, "epochs": 100, "buffer_size": 6000, "noise_dims": 100}
+HYPER = {
+    "batch_size": 128,
+    "epochs": 150,
+    "buffer_size": 6000,
+    "noise_dims": 100,
+    "learning_rate": 0.0002,
+    "beta1": 0.5,
+}
+
+
+def logging_summaries(
+    summary_writer: tf.contrib.summary.SummaryWriter, logged: Dict
+) -> None:
+    """
+    Define a simple logging procedure to be used with AnoGAN stepwise training logging
+    call.
+
+    Args:
+        summary_writer: A `tf.contrib.summary.SummaryWriter` previously instatiated.
+        logged: `Dict` collections of all the variables logged by the training ops.
+
+    Returns:
+        None
+    """
+
+    with summary_writer.as_default(), tf.contrib.summary.always_record_summaries():
+        tf.contrib.summary.image("generated", logged["generated_data"])
+        tf.contrib.summary.image("real", logged["real_data"])
+        tf.contrib.summary.scalar("generator/loss", logged["gen_loss"])
+        tf.contrib.summary.scalar("discriminator/loss", logged["disc_loss"])
 
 
 def load_fashion_mnist_train_dataset(hyper: Dict) -> tf.data.Dataset:
-    (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+    (train_images, train_labels), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
+
+    train_images = train_images.reshape(
+        processed_features.shape[0], *self.image_size
+    ).astype("float32")
+
     # We are normalizing the images to the range of [-1, 1]
     train_images = (train_images - 127.5) / 127.5
 
@@ -24,6 +59,27 @@ def load_fashion_mnist_train_dataset(hyper: Dict) -> tf.data.Dataset:
         .repeat(hyper["epochs"])
     )
     return train_dataset
+
+
+def generator_loss(d_gen):
+    return tf.losses.sigmoid_cross_entropy(tf.ones_like(d_gen), d_gen)
+
+
+def discriminator_loss(d_real, d_gen):
+    # [1,1,...,1] with real output since it is true and we want
+    # our generated examples to look like it
+    real_loss = tf.losses.sigmoid_cross_entropy(
+        multi_class_labels=tf.ones_like(d_real), logits=d_real
+    )
+
+    # [0,0,...,0] with generated images since they are fake
+    generated_loss = tf.losses.sigmoid_cross_entropy(
+        multi_class_labels=tf.zeros_like(d_gen), logits=d_gen
+    )
+
+    total_loss = real_loss + generated_loss
+
+    return total_loss
 
 
 class Generator(tf.keras.Model):
@@ -47,16 +103,118 @@ class Generator(tf.keras.Model):
         x = self.fc1(x)
         x = self.batchnorm1(x, training=training)
         x = tf.nn.relu(x)
-
         x = tf.reshape(x, shape=(-1, 7, 7, 64))
-
         x = self.conv1(x)
         x = self.batchnorm2(x, training=training)
         x = tf.nn.relu(x)
-
         x = self.conv2(x)
         x = self.batchnorm3(x, training=training)
         x = tf.nn.relu(x)
-
         x = tf.nn.tanh(self.conv3(x))
         return x
+
+
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding="same")
+        self.conv2 = tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding="same")
+        self.dropout = tf.keras.layers.Dropout(0.3)
+        self.flatten = tf.keras.layers.Flatten()
+        self.fc1 = tf.keras.layers.Dense(1)
+
+    def call(self, x, training=True):
+        x = tf.nn.leaky_relu(self.conv1(x))
+        x = self.dropout(x, training=training)
+        x = tf.nn.leaky_relu(self.conv2(x))
+        x = self.dropout(x, training=training)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        return x
+
+
+class GAN:
+    def __init__(
+        self, generator: k.models.Model, discriminator: k.models.Model, hyper: Dict
+    ) -> None:
+        learning_rate = hyper["learning_rate"]
+        beta1 = hyper["beta1"]
+        self.generator_optimizer = tf.train.AdamOptimizer(learning_rate, beta1)
+        self.discriminator_optimizer = tf.train.AdamOptimizer(learning_rate, beta1)
+
+        self.generator = generator()
+        self.generator.call = tfe.defun(self.generator.call)
+
+        self.discriminator = discriminator()
+        self.discriminator.call = tfe.defun(self.discriminator.call)
+
+        model_dir = "./logs"
+        self.checkpoint_prefix = os.path.join(model_dir, "ckpt")
+        self.checkpoint = tf.train.Checkpoint(
+            generator_optimizer=self.generator_optimizer,
+            discriminator_optimizer=self.discriminator_optimizer,
+            generator=self.generator,
+            discriminator=self.discriminator,
+        )
+        self.summary_writer = tf.contrib.summary.create_file_writer(
+            model_dir, flush_millis=10000
+        )
+
+    def train(self, dataset, batch_size, noise_dims):
+        global_step = tf.train.get_or_create_global_step()
+        for step, real_data in enumerate(dataset, start=1):
+            noise = tf.random_normal((batch_size, noise_dims))
+
+            # We record all the operations in the tape
+            with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+                generated_data = self.generator(noise, training=True)
+
+                d_real = self.discriminator(real_data, training=True)
+                d_gen = self.discriminator(generated_data, training=True)
+
+                gen_loss = generator_loss(d_gen)
+                disc_loss = discriminator_loss(d_real, d_gen)
+
+            # We retrieve the gradients from our records
+            gradients_of_generator = gen_tape.gradient(
+                gen_loss, self.generator.variables
+            )
+            gradients_of_discriminator = disc_tape.gradient(
+                disc_loss, self.discriminator.variables
+            )
+
+            # Optimize and apply the gradients
+            self.generator_optimizer.apply_gradients(
+                zip(
+                    gradients_of_generator,
+                    self.generator.variables,
+                    global_step=global_step,
+                )
+            )
+            self.discriminator_optimizer.apply_gradients(
+                zip(gradients_of_discriminator, self.discriminator.variables)
+            )
+
+            LOGGED = {
+                "generated_data": generated_data,
+                "real_data": real_data,
+                "gen_loss": gen_loss,
+                "disc_loss": disc_loss,
+            }
+
+            if step % 10 == 0:
+                print(f"--------------------------")
+                print(f"STEP: {step}")
+                print(f"D_LOSS: {disc_loss}")
+                print(f"G_LOSS: {gen_loss}")
+                logging_summaries(self.summary_writer, LOGGED)
+
+
+def main():
+    dataset = load_fashion_mnist_train_dataset(HYPER)
+    gan = GAN(generator=Generator, discriminator=Discriminator, hyper=HYPER)
+    gan.train(dataset, batch_size=HYPER["batch_size"], noise_dims=HYPER["noise_dims"])
+
+
+if __name__ == "__main__":
+    main()
